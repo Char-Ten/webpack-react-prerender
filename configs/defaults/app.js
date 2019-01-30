672d9770b524d8104598b73c97b85405f917a30b
CharTen <645070346@qq.com>
2019-01-30 23:14:30 +0800

    update app.js template

--- configs/defaults/app.js
+++ configs/defaults/app.js
@@ -3,67 +3,28 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = this.match({
-            list: new Array(0).fill(1).map((item, index) => `吃${index}头猪`),
-            title: '2333',
-            time: 0
+            num:200
         });
     }
     match(state) {
-        if (typeof document === 'undefined') {
+        if (module.IS_RENDER) {
             return state;
         }
-        const listElements = document.querySelectorAll('#app li');
+        const p = document.querySelector('#app p')||{};
         return {
             ...state,
-            list: Array.prototype.map.call(listElements, item => item.innerHTML)
+            num:parseInt(p.textContent)
         };
     }
     render() {
         return (
             <div id="app">
-                <ul>
-                    {this.state.list.map((item, i) => {
-                        return (
-                            <li className="test" key={i}>
-                                {item}
-                            </li>
-                        );
-                    })}
-                </ul>
-                <button onClick={this.addItem} >加一头猪</button>
-                <button onClick={this.removeItem} >拉一坨屎</button>
-                <p>{this.state.time}</p>
+                <h1>test</h1>
+                <p>{this.state.num}</p>
+                <button onClick={()=>this.setState({num:this.state.num+1})}>+1</button>
+                <button onClick={()=>this.setState({num:this.state.num-s1})}>-1</button>
             </div>
         );
     }
-
-    addItem = () => {
-        let time = new Date().getTime();
-        this.setState(
-            {
-                list: [...this.state.list, `吃${this.state.list.length}头猪`]
-            },
-            () => {
-                this.setState({
-                    time: new Date().getTime() - time
-                });
-            }
-        );
-    };
-
-    removeItem = () => {
-        let time = new Date().getTime();
-        this.state.list.pop();
-        this.setState(
-            {
-                list: this.state.list
-            },
-            () => {
-                this.setState({
-                    time: new Date().getTime() - time
-                });
-            }
-        );
-    };
 }
 
